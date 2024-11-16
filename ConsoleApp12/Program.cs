aebd62740fb3e935cbb6dce6370558b845302f5f
yulia <yuliia_yaresko@knu.ua>
2024-11-16 14:14:45 +0200

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NumberInputConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть ціле число: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine($"Ви ввели число {number}");
+            }
+            else
+            {
+                Console.WriteLine("Помилка: введене значення не є цілим числом.");
+            }
+        }
+    }
+}
+
